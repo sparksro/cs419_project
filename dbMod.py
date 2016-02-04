30433f9fcf67e9170ce584a706702c7c95005c16
leehkl <slee.hkl@gmail.com>
2016-02-04 01:00:27 -0800

    Fix to time -> exclude seconds

--- dbMod.py
+++ dbMod.py
@@ -4,23 +4,21 @@
 # version 1.00
 
 import MySQLdb
-import mysql.connector
-from  mysql.connector import errorcode
+#import mysql.connector
+#from  mysql.connector import errorcode
 import datetime
 import time
+import sys
 
 #opens a connection with advising database - returns connection object
+#if connection error - returns 1
 def make_connection():
     try:
         db = MySQLdb.connect("52.10.233.116","advising_user", "VLrMMJSScH6ZLHca",  "advising_db")
         varConnected = True;
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password %d" % (err.errno))
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
+    except MySQLdb.Error as e:
+        print(e)
+        return 1
     return db
 
 #disconnects connection to database - pass in connection object
@@ -44,14 +42,14 @@ def GetAllAppts(db, FacultyEmail):
             catch = cursor.fetchall()
             for r in catch:
               #format time object
-              STime = str(r[7])
-              ':'.join(str(STime).split(':')[:1])
-              ETime = str(r[8])
-              ':'.join(str(ETime).split(':')[:1])
-              results.append({"Id": r[0], "FacultyName": r[1], "FacultyEmail": r[2], \
-                  "StudentName":r[3], "StudentEmail":r[4], "Date": r[5].strftime('%m/%d/%y'), "Status": r[6], "StartTime": STime, "EndTime": ETime}) 
+              #source: stackoverflow.com/questions/904746/how-to-remove-all-characters-after-a-specific-character-in-python
+              STime = ':'.join(str(r[7]).split(':')[:-1])
+              ETime = ':'.join(str(r[8]).split(':')[:-1])
+              results.append({"Id": str(r[0]), "FacultyName": r[1], "FacultyEmail": r[2], "StudentName":r[3],
+                  "StudentEmail":r[4], "Date": r[5].strftime('%m/%d/%y'), "Status": r[6], "StartTime": STime, "EndTime": ETime}) 
     except:
         print"Error: Unable to fetch data: "  + sql
+        return
 
     return results
 
@@ -94,28 +92,37 @@ def UpdateAppt(db, Status, ApptID):
         db.rollback()
         print"Error: Unable to update appointment: " +sql
 
+#-------------------------------
+#TESTING - remove in production
+#--------------------------------
 
-#TESTING
 db = make_connection()
-#TO DO: convert all emails to upper or lower
+if db == 1:
+    print "Error with connection.  Goodbye"
+    sys.exit()
+
+#Values to insert to DB
+#Must convert all emails to upper to bypass case-sensitive requests
 FacultyName = "Mr.T"
 FacultyEmail= "MrT@oregonstate.edu"
 StudentName = "JoeT"
 StudentEmail = "JoeT@oregonstate.edu"
-Date = datetime.datetime(2016, 2, 3)
-print Date
+Date = "February 4, 2016"
+Date = datetime.datetime.strptime(Date, "%B %d, %Y")
 Status = "Pending"
 StartTime = datetime.time(10, 30)
 EndTime = datetime.time(11, 15)
 #InsertNewAppt(db, FacultyName, FacultyEmail, StudentName, StudentEmail, Date, Status, StartTime, EndTime)
-ApptID = 7
-#DeleteAppt(db,ApptID)
+
+#set ID for appointment to modify
+ApptID = 6 
 Status = "Accepted"
-UpdateAppt(db, Status, ApptID)
-appts = GetAllAppts(db, 'MrT@oregonstate.edu')
-print appts
 
+#DeleteAppt(db,ApptID)
+#UpdateAppt(db, Status, ApptID)
 
+#appts = GetAllAppts(db, FacultyEmail)
+#print appts
 
 disconnect(db)
 
